de0d3dbb5071b7939b264ec82f1f427b7413a616
ander.areizagab <ander.areizagab@103L9.opendeusto.es>
2015-11-16 10:28:22 +0100

    musica

--- SnakeV2/src/Ventana.java
+++ SnakeV2/src/Ventana.java
@@ -1,4 +1,12 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -26,6 +34,8 @@ public class Ventana extends JFrame{
 			
 			Ventana v =new Ventana();
 			v.add(new MainMenu(v));
+			sonido1 s = new sonido1();
+			s.start();
 			long time=System.currentTimeMillis();
 			do{
 				if(System.currentTimeMillis()-time>16.7){
@@ -40,4 +50,25 @@ public class Ventana extends JFrame{
 			}while(v.isActive());
 			
 		}
+}
+class sonido1 extends Thread{
+	
+	void Sonido() throws LineUnavailableException, IOException, UnsupportedAudioFileException{
+		BufferedInputStream bis = new BufferedInputStream(getClass().getResourceAsStream("Cancion.wav"));
+		AudioInputStream ais = AudioSystem.getAudioInputStream(bis);
+		Clip sonido = AudioSystem.getClip();
+		sonido.open(ais);
+		sonido.loop(5);
+		
+	}
+	@Override
+	public void run() {
+		try {
+			this.Sonido();
+		} catch (LineUnavailableException | IOException
+				| UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
