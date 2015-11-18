424b7d8306d198b8d0f899ae4ed633cc40cce6af
iñigo Alonso <iigo Alonso@192.168.1.39>
2015-11-18 21:58:20 +0100

    Escalar imagenes y pantalla

---
+++ SnakeV2/src/CODIGO_ESCALABLE.java
@@ -0,0 +1,18 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+
+
+public class CODIGO_ESCALABLE {
+//Aviso, convendria empezar a hacer carpetas -> dejo mis propuestas 
+	
+	
+	//Tamaño de la ventana
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	double width = screenSize.getWidth();
+	double height = screenSize.getHeight();
+	
+	//escalar imagen
+	this.imageSuelo = new ImageIcon("recursos/suelo.jpg").getImage().getScaledInstance(ANCHURASUELO, ALTURASUELO,  java.awt.Image.SCALE_SMOOTH); // transformarlo y escalarlo de forma delicada
+}
