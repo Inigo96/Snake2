3c8620952915333a311f08f850a74ff1c121b0f7
iñigo Alonso <iigo Alonso@192.168.1.37>
2015-11-28 16:58:48 +0100

    Arreglo errores por no-static

--- SnakeV2/src/MainMenu.java
+++ SnakeV2/src/MainMenu.java
@@ -42,8 +42,8 @@ public class MainMenu extends JPanel{
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				Ventana.s.n=false;
-				Ventana.s.sonido.stop();
+				v.s.n=false;
+				v.s.sonido.stop();
 				v.dispose();
 				
 			}
