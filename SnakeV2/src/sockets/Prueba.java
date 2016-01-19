eb180bb87ce7cd5bd04d2153ee75a67a47830159
inigo.alon <inigo.alon@103L9.opendeusto.es>
2016-01-19 17:10:18 +0100

    gfgf

---
+++ SnakeV2/src/sockets/Prueba.java
@@ -0,0 +1,14 @@
+package sockets;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Prueba {
+public static void main(String[] args) {
+	String a="00eee";
+	System.out.println(a.substring(0,2));
+	System.out.println(a.substring(2,a.length()));
+	
+	JOptionPane.showMessageDialog(new JFrame(), "1.-feo\n2.1rr");					
+}
+}
